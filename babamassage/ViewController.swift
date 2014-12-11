1fe09317144679eb2f6fbf7a49a8835c7cf4292f
Karen Wu <karenwu@Karens-MacBook-Pro.local>
2014-12-11 15:11:08 -0800

    Initial Commit

---
+++ babamassage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  babamassage
+//
+//  Created by Karen Wu on 12/11/14.
+//  Copyright (c) 2014 Karen Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
